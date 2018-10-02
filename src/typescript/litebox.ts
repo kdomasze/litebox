771848d5406d82ad3d456486c98df0634eae932b
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-10-02 13:30:22 -0700

    Adds class to define litebox along with display optionsoptions include the following:-Show exit button (default: true)-Enable esc exit (default: true)-Fade in time (default: 300ms)-Fade out time (default 300ms)

--- src/typescript/litebox.ts
+++ src/typescript/litebox.ts
@@ -1,112 +1,148 @@
-// TODO: create object to house options for litebox
-// probably move all functions into a class
+class Litebox {
+    private litebox: HTMLElement | null = null;
+    private options = {
+        enableEscClose: true,
+        enableExitButton: true,
+        fadeInTime: 300,
+        fadeOutTime: 300
+    };
+
+    invoke = (anchor: Element) => {
+        // get path to full image
+        let imageSrc: string = '';
+        if (
+            anchor.getAttribute('href') !== null &&
+            (anchor.getAttribute('href') as string) !== ''
+        ) {
+            imageSrc = anchor.getAttribute('href') as string;
+        }
+
+        // get alt text
+        const imgChild = anchor.querySelector('img');
+        let imageAlt: string = '';
+        if (imgChild !== null && imgChild.getAttribute('alt') !== null) {
+            imageAlt = imgChild.getAttribute('alt') as string;
+        }
+
+        // build litebox
+        const liteboxTemplate = document.querySelector('#template-litebox');
+
+        let output: string = '';
+        if (liteboxTemplate === null) return;
+
+        output = liteboxTemplate.innerHTML
+            .replace(/{{image_src}}/g, imageSrc)
+            .replace(/{{image_alt}}/g, imageAlt);
+
+        // append litebox to body
+        const body = document.querySelector('body');
+        if (body === null) return;
+        body.insertAdjacentHTML('beforeend', output);
+
+        this.litebox = document.getElementById('js-litebox');
+        if (this.litebox === null) return;
+
+        // close litebox with x
+        if(this.options.enableExitButton) {
+            const exit = this.litebox.querySelector('#js-litebox-exit');
+            if (exit === null) return;
+            exit.addEventListener('click', () => {
+                this.exit(this.litebox as HTMLElement);
+            });
+        } else {
+            const exit = this.litebox.querySelector('#js-litebox-exit');
+
+            if(exit === null) return;
+            const parent = exit.parentNode;
+
+            if(parent == null) return;
+            parent.removeChild(exit);
+            document.removeEventListener('keyup', this.escapeEvent);
+        }
+
+        // close litebox with escape
+        if(this.options.enableEscClose) {
+            document.addEventListener('keyup', this.escapeEvent);
+        }
+
+        fadeIn(this.litebox, this.options.fadeInTime);
+    }
+
+    exit = (litebox: HTMLElement) => {
+        if(litebox === null) return;
+
+        const parent = litebox.parentNode;
+        if (parent === null) return;
+
+        fadeOut(litebox, this.options.fadeOutTime, () => {
+            parent.removeChild(litebox);
+        });
+        this.litebox = null;
+    }
+
+    private escapeEvent = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            this.exit(this.litebox as HTMLElement);
+        }
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const anchors: NodeListOf<Element> = document.querySelectorAll('a[data-litebox]');
+
+    const litebox: Litebox = new Litebox();
 
     anchors.forEach((anchor) => {
         anchor.addEventListener('click', (event) => {
             // prevents the anchor from redirecting
             event.preventDefault();
-
-            // get path to full image
-            let imageSrc: string = '';
-            if (
-                anchor.getAttribute('href') !== null &&
-                (anchor.getAttribute('href') as string) !== ''
-            ) {
-                imageSrc = anchor.getAttribute('href') as string;
-            }
-
-            // get alt text
-            const imgChild = anchor.querySelector('img');
-            let imageAlt: string = '';
-            if (imgChild !== null && imgChild.getAttribute('alt') !== null) {
-                imageAlt = imgChild.getAttribute('alt') as string;
-            }
-
-            // build litebox
-            buildLitebox(imageSrc, imageAlt);
+            litebox.invoke(anchor);
             return;
         });
     });
 });
 
-const buildLitebox = (imageSrc: string, imageAlt: string) => {
-    const liteboxTemplate = document.querySelector('#template-litebox');
-
-    let output: string = '';
-    if (liteboxTemplate !== null) {
-        output = liteboxTemplate.innerHTML
-            .replace(/{{image_src}}/g, imageSrc)
-            .replace(/{{image_alt}}/g, imageAlt);
-    }
-
-    // append litebox to body
-    const body = document.querySelector('body');
-    if (body !== null) {
-        body.insertAdjacentHTML('beforeend', output);
-    }
-
-    addLiteboxEventListeners();
-};
-
-const addLiteboxEventListeners = () => {
-    const litebox = document.getElementById('js-litebox');
-
-    // close litebox with escape
-    if (litebox !== null) {
-        document.addEventListener('keyup', (event) => {
-            if (event.key === 'Escape') {
-                exitLitebox(litebox);
-            }
-        });
-
-        // close litebox with x
-        const exit = litebox.querySelector('#js-litebox-exit');
-        if (exit !== null) {
-            exit.addEventListener('click', (event) => {
-                exitLitebox(litebox);
-            });
-        }
-
-        fadeIn(litebox);
-    }
-};
-
-const exitLitebox = (litebox: HTMLElement) => {
-    const parent = litebox.parentNode;
-    if (parent !== null) {
-        fadeOut(litebox, () => parent.removeChild(litebox));
-    }
-};
-
-const fadeIn = (element: HTMLElement) => {
+const fadeIn = (element: HTMLElement, duration: number) => {
     element.style.opacity = '0';
     element.style.display = 'block';
 
+    let end = +new Date() + duration;
+
     (function fade() {
-        let value = parseFloat(element.style.opacity);
-        value += 0.1;
-        if(!(value > 1)) {
-            element.style.opacity = value.toString();
-            requestAnimationFrame(fade);
+        let current = +new Date();
+        let remaining = end - current;
+
+        if(remaining < 60) {
+            element.style.opacity = '1';
+            return;
+        } else {
+            let rate = 1 - remaining / duration;
+            element.style.opacity = rate.toString();
         }
+
+        requestAnimationFrame(fade);
     })();
 }
 
-const fadeOut = (element: HTMLElement, callback: () => any) => {
+const fadeOut = (element: HTMLElement, duration: number, callback: () => any) => {
     element.style.opacity = '1';
 
+    let end = +new Date() + duration;
+
     (function fade() {
-        let value = parseFloat(element.style.opacity);
-        value -= 0.1;
-        if(value < 0) {
+        let current = +new Date();
+        let remaining = end - current;
+
+        if(remaining < 60) {
+            element.style.opacity = '0';
             element.style.display = 'none';
             callback();
+            return;
         } else {
-            element.style.opacity = value.toString();
-            requestAnimationFrame(fade);
+            let rate = 1 - remaining / duration;
+            element.style.opacity = (1 - rate).toString();
         }
+
+        requestAnimationFrame(fade);
     })();
 }
